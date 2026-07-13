2b78ed8f13e2850af128553387101678734e9194
“Zaamaar” <ayotomiwaobadare1@gmail.com>
2026-07-13 19:28:05 +0100

    Point frontend at HTTPS custom domain backend

--- config.js
+++ config.js
@@ -1,3 +1,1 @@
-// Point this at your deployed API's ALB DNS name (or localhost while developing).
-// Example once deployed: "http://cinetrack-alb-123456789.us-east-1.elb.amazonaws.com"
-const API_BASE_URL = "http://cinetrack-alb-598407360.us-east-1.elb.amazonaws.com";
+const API_BASE_URL = "https://api.hngayotomiwa.online"\;
